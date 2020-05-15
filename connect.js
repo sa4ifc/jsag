f406e789cdaecfab3deb7f172cd18a7153c6ffd5
sa4ifc <65377603+sa4ifc@users.noreply.github.com>
2020-05-15 03:35:19 +0200

    Add files via upload

---
+++ connect.js
@@ -0,0 +1,14 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBpM9Gk7fMMbxzXoqAsFZLb9eHg1eRPwWg",
+    authDomain: "posts-47acf.firebaseapp.com",
+    databaseURL: "https://posts-47acf.firebaseio.com",
+    projectId: "posts-47acf",
+    storageBucket: "posts-47acf.appspot.com",
+    messagingSenderId: "485217301114",
+    appId: "1:485217301114:web:fc5922bac374f873b101c3",
+    measurementId: "G-NVDSD9KTT8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
